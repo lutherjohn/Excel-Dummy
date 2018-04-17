96f18d481ce3d66b93e5dcb462f44e231ed051b7
lothux1987 <14127085+lutherjohn@users.noreply.github.com>
2018-04-17 10:08:51 +0400

    Update sample_data.py

--- sample_data.py
+++ sample_data.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
 
-url = 'https://github.com/lutherjohn/Excel-Dummy/blob/master/student.xlsx?raw=true'
+excel_url = 'https://github.com/lutherjohn/Excel-Dummy/blob/master/student.xlsx?raw=true'
+csv_url   = 'https://github.com/lutherjohn/Excel-Dummy/blob/master/student.csv?raw=true'
 
+#url = 'https://github.com/lutherjohn/Excel-Dummy/blob/master/student.xlsx?raw=true'
+#at the end of the file extention of your excel file or csv ex: sample.xlsx?raw=true, sample.csv?raw=true
 
-df = pd.read_excel(url,sheetname=0,header=1)
-w = df.head(5)
+excel_df = pd.read_excel(excel_url)
+excel = excel_df.head(3)
 
-print(w)
+df = pd.read_csv(csv_url)
+w = df.head(3)
+
+print(excel)
