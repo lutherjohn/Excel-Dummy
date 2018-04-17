76271779d51db2246a1f811e93dea876b0bb1ccd
lothux1987 <14127085+lutherjohn@users.noreply.github.com>
2018-04-17 10:16:37 +0400

    Update sample_data.py

--- sample_data.py
+++ sample_data.py
@@ -5,7 +5,8 @@ excel_url = 'https://github.com/lutherjohn/Excel-Dummy/blob/master/student.xlsx?
 csv_url   = 'https://github.com/lutherjohn/Excel-Dummy/blob/master/student.csv?raw=true'
 
 #url = 'https://github.com/lutherjohn/Excel-Dummy/blob/master/student.xlsx?raw=true'
-#at the end of the file extention of your excel file or csv ex: sample.xlsx?raw=true, sample.csv?raw=true
+#at the end of the file extention of your excel file or csv set the raw to true for python to read the files from github repo.
+#ex: sample.xlsx?raw=true, sample.csv?raw=true
 
 excel_df = pd.read_excel(excel_url)
 excel = excel_df.head(3)
